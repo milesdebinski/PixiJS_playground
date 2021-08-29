836dc980ed3ecb503c32c1096a6632f6aed12c89
Miles Debinski <miles.debinski@gmail.com>
2021-08-29 17:08:21 +0100

    add basic keyboard movement

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let app;
 let player;
+let keys = {};
+let keysDiv;
 
 window.onload = () => {
   app = new PIXI.Application({
@@ -18,15 +20,44 @@ window.onload = () => {
   player.y = app.view.height / 2;
 
   stage.addChild(player);
-
   // mouse interactions
   stage.interactive = true;
-  stage.on("pointermove", movePlayer);
+  // stage.on("pointermove", movePlayer);
+
+  // keyboard event handlers
+  window.addEventListener("keydown", keysDown);
+  window.addEventListener("keyup", keysUp);
+
+  app.ticker.add(gameLoop);
+  keysDiv = document.querySelector("#keys");
+};
+
+const keysDown = (e) => {
+  keys[e.keyCode] = true;
+};
+const keysUp = (e) => {
+  keys[e.keyCode] = false;
+};
+
+const gameLoop = () => {
+  keysDiv.innerHTML = JSON.stringify(keys);
+  const speed = 5;
+  if (keys[37]) {
+    player.x -= speed;
+  }
+  if (keys[38]) {
+    player.y -= speed;
+  }
+  if (keys[39]) {
+    player.x += speed;
+  }
+  if (keys[40]) {
+    player.y += speed;
+  }
 };
 
 const movePlayer = (e) => {
   let position = e.data.global;
-
   player.x = position.x;
   player.y = position.y;
 };
